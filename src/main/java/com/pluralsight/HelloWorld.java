4c603a46c93dce8826c87d1d4318289048a4c70c
Your Name <youremail@example.com>
2026-04-06 14:47:06 -0400

    Initial commit

---
+++ src/main/java/com/pluralsight/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("I am Adam!");
+        System.out.println("Learning Java!");
+        System.out.println("Let's Go!");
+    }
+}
